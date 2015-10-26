89e298d06b0cfc4430b82f4b5b259b3de3cbc226
Tordingo <nikbolotin@yandex.ua>
2015-10-26 21:55:01 +0200

    part of code

--- Sorting/BucketSort.py
+++ Sorting/BucketSort.py
@@ -1,3 +1,21 @@
 class BucketSortImpl:
     def sort(self, unsorted_array):
-        return 'hello world'
+
+        if not unsorted_array:
+            return []
+
+        minValue = [0];
+        maxValue = 0;
+        for item in unsorted_array:
+            if item < minValue:
+                minValue = item
+
+            if item > maxValue:
+                maxValue = item
+
+        bucketList = xrange(maxValue - minValue + 1);
+
+        for number in unsorted_array:
+            bucketList[number - minValue].add(number)
+
+        
